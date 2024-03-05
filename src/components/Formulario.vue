c7e86c0d3f9abe46eb23e8e83fb1134bd9dbd1d0
Bryan Gallo <bryangallo95@hotmail.com>
2024-03-04 23:59:57 -0500

    Componente Formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">
+            Seguimiento de Pacientes
+        </h2>
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade Pacientes y
+            <span class="text-indigo-600">Administralos</span>
+        </p>
+    </div>
+</template>
